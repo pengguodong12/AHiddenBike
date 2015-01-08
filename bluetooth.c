977aab8ea98896be38419cbfe553378319ff6f6c
Guodong Peng <pengguodong12@gmail.com>
2015-01-08 23:53:24 +0800

    C code for MSP430G2553
    I have added the function of music playing to the MCU system in replace
    of the boring buzzi. To accomplish this, I have to use a timer to
    generate PWM pulse output to produce tunes with different frequencies by
    which it can play music. Since there are only two timers on MSP430G2553
    with PWM function and they are used for timing before, I replace one of
    them with Watch Dog Timer which also provide a timing function and  but
    not PWM. The timer spared is used for playing music.

--- bluetooth.c
+++ bluetooth.c
@@ -1,10 +1,29 @@
 ﻿//P2.0--S1，P2.4--DS，P2.5--SHCP，P2.6--STCP，控制最右边的数码管显示接收到的消息
 //P1.1--RXD接蓝牙txd，P1.2--TXD 接蓝牙rxd
-//P1.5--L6、P1.6--L7/Buzz
-//采用TA0中断方式对LED亮的时间进行计时控制
-//采用TA1中断方式对Buzz蜂鸣的时间进行计时控制
+//P1.5--L6、P1.6--L7、P2.1--Buzz,蜂鸣采用TA1输出的PWM来播放音乐
+//采用TA0中断方式对LED亮的时间进行计时控制——10s
+//采用WDT中断方式对Buzz蜂鸣的时间进行计时控制——20s
+//采用TA1输出PWM从而输出音乐
 #include "io430.h"
 #include "in430.h"
+
+const unsigned char music[2][34]={{6,3,2,3,2,2,3,3,1,2,3,6,1,6,1,6,6,6,1,1,6,1,2,2,4,4,4,1,1,2,3,3,3,3},
+								  {3,2,3,1,1,7,5,5,6,0,1,2,1,3,0,3,3,3,3,3,3,4,3,2,1,0,5,6,4,3,1,3,2,2}};  //储存歌曲1、2音符唱名
+						 
+const unsigned char lvl[2][34]={{1,2,2,2,2,2,2,2,2,2,2,1,2,1,2,1,1,1,2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2},
+								{4,4,4,4,4,2,2,2,2,1,4,4,4,4,1,4,4,4,4,4,4,4,4,4,4,1,2,2,4,4,4,4,4,4}};  //储存歌曲1、2音阶，低音1，中音2，高音3
+						 
+const unsigned char beat[2][34]= {{2,1,1,1,1,1,1,4,1,1,1,1,1,1,1,1,6,1,1,1,1,1,1,6,1,1,1,1,1,1,6,0,0,0},
+								  {1,3,1,3,1,2,1,1,1,2,1,2,2,9,2,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,9}};  //储存歌曲1、2音符节拍数
+
+const char LEDtab[16]={0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92,
+						0x82, 0xF8,0x80, 0x90, 0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E};//数码管显示的数字编码
+						 
+
+unsigned char BUZZ_FLAG = 0;		//播放音乐标志
+
+//接收到的单个字符
+unsigned char buffer;
 
 //蓝牙异步串口通信初始化
 void USCIA0_init(void);
@@ -15,14 +34,14 @@ void CLK_init(void);
 //TA0初始化，并开始计数
 void init_TA0(void);
 
-//TA1初始化，并开始计数
-void init_TA1(void);
+//WDT初始化，并开始计数
+void init_WDT(void);
 
 //TA0停止计数
 void stop_TA0(void);
 
-//TA1停止计数
-void stop_TA1(void);
+//停止WDT的计数
+void stop_WDT(void);
 
 //灯、蜂鸣器初始化
 void Buzz_init(void);
@@ -33,18 +52,18 @@ void Tube_init(void);
 //数码管显示接收的蓝牙消息，num表示要显示的数字
 void display(unsigned char num);
 
-//接收到的单个字符
-unsigned char buffer;
+//以音阶lv1来播放音符k
+void play (unsigned char k,unsigned char lvl);
 
-//数码管显示的数字编码
-const char LEDtab[16]={0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92,
-0x82, 0xF8,0x80, 0x90, 0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E};
+//延时函数
+void delay(unsigned char b);
 
 int main( void )
 {
-	WDTCTL = WDTPW + WDTHOLD;//关闭看门狗
-	char temp = 0xff; 		//代表蓝牙传给单片机的字符
 
+	unsigned char i;
+	WDTCTL = WDTPW + WDTHOLD;//关闭看门狗
+	
 	__disable_interrupt();	//关总中断
 
 	CLK_init();				//时钟源初始化
@@ -56,38 +75,20 @@ int main( void )
 	LPM0;					//进入低功耗
 	while(1)
 	{
-		temp = buffer;
-
-		switch (temp) 		//根据接收的不同消息进行不同操作
-		{
-		case 1:          	//亮灯
-			P1OUT &= ~BIT5;
-			UCA0TXBUF = 1;	//回传成功亮灯信号
-			init_TA0();		//开始计时10s，10s后自动灭灯
-			break;
-		case 2:          	//灭灯
-			stop_TA0();		//停止计时
-			P1OUT |= BIT5;
-			UCA0TXBUF = 2;	//回传成功灭灯信号
-			
-			break;
-		case 3:
-			P1OUT &= ~BIT6;	//发声
-			UCA0TXBUF = 3;	//回传成功发声信号
-			init_TA1();		//开始计时10s，10s后自动关声
-			break;
-		case 4:
-			stop_TA1();		//停止计时
-			P1OUT |= BIT6;	//关声
-			UCA0TXBUF = 4;	//回传成功关声信号
-			
-			break;
-		default:
-			break;
+		while (BUZZ_FLAG==3 || BUZZ_FLAG==9) {
+			for (i=0; i<34; i++) {
+				if (BUZZ_FLAG==3){			//播放音乐1
+					play(music[0][i],lvl[0][i]);
+					delay(beat[0][i]);
+				}
+				else if (BUZZ_FLAG == 9){		//播放音乐2
+					play(music[1][i],lvl[1][i]);
+					delay(beat[1][i]);
+				}
+				else break;
+			}
 		}
-		display(temp);		//数码管显示接收的蓝牙消息
-		temp =	0xff;
-		IE2 |= UCA0RXIE;	//打开接收中断
+		
 		LPM0;				//进入低功耗
 	}
 
@@ -97,27 +98,64 @@ int main( void )
 //蓝牙接收数据中断
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void UCA0RX_isr(void) {
-	buffer = UCA0RXBUF;		//接收一个字符并保存
 	IE2 &=~UCA0RXIE;		//关闭接收中断
-	LPM0_EXIT;				//退出低功耗
+	buffer = UCA0RXBUF;		//接收一个字符并保存
+	display(buffer);		//数码管显示接收的蓝牙消息
+		
+	switch (buffer) 		//根据接收的不同消息进行不同操作
+	{
+	case 1:          	//亮灯
+		P1OUT &= ~(BIT5+BIT6);
+		init_TA0();		//开始计时10s，10s后自动灭灯
+		UCA0TXBUF = 1;	//回传成功亮灯信号
+		break;
+	case 2:          	//灭灯
+		stop_TA0();		//停止计时
+		P1OUT |= (BIT5+BIT6);
+		UCA0TXBUF = 2;	//回传成功灭灯信号
+		break;
+	case 3:				//歌曲1
+		BUZZ_FLAG = 3;	//歌曲1播放
+		init_WDT();		//计时20s，20s后自动关音乐
+		UCA0TXBUF = 3;	//回传成功播放歌曲1信号
+		LPM0_EXIT;		//退出低功耗
+		break;
+	case 9:				//歌曲2
+		BUZZ_FLAG = 9;	//歌曲2播放
+		init_WDT();		//计时20s，20s后自动关音乐
+		UCA0TXBUF = 9;	//回传成功播放歌曲2信号
+		LPM0_EXIT;		//退出低功耗
+		break;
+	case 4:				//关闭歌曲
+		stop_WDT();
+		play(0,1);		//关声
+		BUZZ_FLAG = 4;	//歌曲播放禁止
+		UCA0TXBUF = 4;	//回传成功关声信号
+		LPM0_EXIT;		//退出低功耗
+		break;
+	default:
+		break;
+	}
+	IE2 |= UCA0RXIE;	//打开接收中断
 }
 
 //TimerA0 CCR0中断
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void timer0_A0_isr(void) {
 	stop_TA0();				//停止TA0的计数
-	P1OUT |= BIT5;			//灭灯
+	P1OUT |= (BIT5+BIT6);	//灭灯
 	UCA0TXBUF = 2;			//回传成功灭灯信号
 	TA0CCTL0 &= ~CCIFG;		//中断标志清零
 }
 
-//TimerA1 CCR0中断
-#pragma vector=TIMER1_A0_VECTOR
-__interrupt void timer0_A1_isr(void) {
-	stop_TA1();				//停止TA1的计数
-	P1OUT |= BIT6;			//关声
+//WDT中断
+#pragma vector=WDT_VECTOR
+__interrupt void WDT_isr(void) {
+	stop_WDT();				//停止WDT的计数
+	BUZZ_FLAG = 4;			//歌曲播放禁止
+	play(0,1);				//关声
 	UCA0TXBUF = 4;			//回传成功关声信号
-	TA1CCTL0 &= ~CCIFG;		//中断标志清零
+	IFG1 &= ~BIT0;			//中断标志清零
 }
 
 // 时钟源初始化
@@ -127,7 +165,7 @@ void CLK_init(void) {
     DCOCTL=CALDCO_12MHZ; 
 	
 	BCSCTL3 |= LFXT1S_2;    //ACLK选择12kHZ的VLOCLK
-	BCSCTL1 |= DIVA_0;		//ACLK 1分频	
+	BCSCTL1 |= DIVA_3;		//ACLK 8分频，即1500Hz
 }
 
 //蓝牙异步串口初始化
@@ -146,37 +184,35 @@ void USCIA0_init(void){
 
 //TA0初始化，并开始计数
 void init_TA0(void) {
-	//清TA0R=0, ACLK为计数时钟，增计数方式，4分频，即频率为3000Hz
+	//清TA0R=0, ACLK为计数时钟，增计数方式，1分频，即频率为3000Hz
 	TA0CTL |= TACLR;
-    TA0CTL |= TASSEL_1 + MC_1 + ID_2;
-	TA0CCR0=30000;				//置TA0CCR0值，10秒
+    TA0CTL |= TASSEL_1 + MC_1 + ID_0;
+	TA0CCR0=15000;				//置TA0CCR0值，10秒
 	TA0CCTL0 |= CCIE;			//允许CCR0中断
 }
 
-//TA1初始化，并开始计数
-void init_TA1(void) {
-	//清TA1R=0, ACLK为计数时钟，增计数方式，4分频，即频率为3000Hz
-	TA1CTL |= TACLR;
-    TA1CTL |= TASSEL_1 + MC_1 + ID_2;
-	TA1CCR0=30000;				//置TA1CCR0值，10秒
-	TA1CCTL0 |= CCIE;			//允许CCR0中断
+//WDT初始化，并开始计数
+void init_WDT(void) {
+	//计数清零、中断计时模式、时钟源为ACLK、计数到32768发起中断，即20s
+	WDTCTL = (WDTPW + WDTCNTCL + WDTTMSEL + WDTSSEL);
+	IE1 |= BIT0;	//WDT分中断允许
 }
 
 //停止TA0的计数
 void stop_TA0(void){
 	//清TA0R=0, 不计数，关闭CCR0中断
 	TA0CCTL0 &= ~CCIE;
-        TA0CTL &= ~(MC0+MC1);
-    	TA0CTL |= TACLR;
+	TA0CTL &= ~(MC0+MC1);
+	TA0CTL |= TACLR;
 }
 
-//停止TA1的计数
-void stop_TA1(void){
-	//清TA1R=0, 不计数，关闭CCR0中断
-	TA1CCTL0 &= ~CCIE;
-    TA1CTL &= ~(MC0+MC1);
-    	TA1CTL |= TACLR;
+//停止WDT的计数
+void stop_WDT(void){
+	IE1 &= ~BIT0;	//WDT分中断禁止
+	//关闭看门狗
+	WDTCTL = WDTPW + WDTHOLD;
 }
+
 
 //LED、Buzz初始化
 void Buzz_init(void) {
@@ -184,6 +220,13 @@ void Buzz_init(void) {
 	P1SEL2 &= ~(BIT5+BIT6);
 	P1OUT |= (BIT5+BIT6);
 	P1DIR |= (BIT5+BIT6);
+	
+	//置P2.1输出PWM
+	P2SEL |= BIT1;
+	P2SEL2 &= ~BIT1;
+	P2DIR |= BIT1; 
+	TA1CTL = TACLR + TASSEL_2 + MC_0 + ID_2;	//清TAR为0，停止计数，时钟为SMCLK，4分频，即3MHz
+	TA1CCTL1 = (OUTMOD_0 + OUT);  //P2.1输出1
 }
 
 //数码管初始化
@@ -210,4 +253,71 @@ void display(unsigned char num){
 	P2OUT |= BIT6;									//给STCP一个脉冲，使8位数据输出
 	P2OUT &= (~BIT6);
 	P2OUT |= BIT0;									//选择最右边的数码管
+}
+
+//以音阶lv1来播放音符k
+void play (unsigned char k,unsigned char lvl) {
+	switch (k)
+	{
+	case 0: // 休止符
+		TA1CCTL1 = (OUTMOD_0 + OUT);  //P2.1输出1
+		TA1CTL = TACLR + TASSEL_2 + MC_0 + ID_2;	//清TAR为0，停止计数，时钟为SMCLK，4分频，即3MHz
+		break;
+	case 1: //do
+		TA1CCR0 = 11364/lvl;
+		TA1CCR1 = (unsigned int)(TA1CCR0/100.0*98.0);	//占空比98%
+		TA1CCTL1 |= OUTMOD_6;  							//Toggle/set输出模式
+		TA1CTL = TACLR + TASSEL_2 + MC_1 +ID_2;	//清TAR为0，增计数，时钟为SMCLK，4分频，即3MHz
+		break;
+	case 2: //re
+		TA1CCR0=10101/lvl;
+		TA1CCR1 = (unsigned int)(TA1CCR0/100.0*98.0);	//占空比98%
+		TA1CCTL1 |= OUTMOD_6;  							//Toggle/set输出模式
+		TA1CTL = TACLR + TASSEL_2 + MC_1 +ID_2;	//清TAR为0，增计数，时钟为SMCLK，4分频，即3MHz
+		break;
+	case 3: //mi
+		TA1CCR0=9091/lvl;
+		TA1CCR1 = (unsigned int)(TA1CCR0/100.0*98.0);	//占空比98%
+		TA1CCTL1 |= OUTMOD_6;  							//Toggle/set输出模式
+		TA1CTL = TACLR + TASSEL_2 + MC_1 +ID_2;	//清TAR为0，增计数，时钟为SMCLK，4分频，即3MHz
+		break;
+	case 4: //fa
+		TA1CCR0=8523/lvl;
+		TA1CCR1 = (unsigned int)(TA1CCR0/100.0*98.0);	//占空比98%
+		TA1CCTL1 |= OUTMOD_6;  							//Toggle/set输出模式
+		TA1CTL = TACLR + TASSEL_2 + MC_1 +ID_2;	//清TAR为0，增计数，时钟为SMCLK，4分频，即3MHz
+		break;
+	case 5: //so
+		TA1CCR0=7576/lvl;
+		TA1CCR1 = (unsigned int)(TA1CCR0/100.0*98.0);	//占空比98%
+		TA1CCTL1 |= OUTMOD_6;  							//Toggle/set输出模式
+		TA1CTL = TACLR + TASSEL_2 + MC_1 +ID_2;	//清TAR为0，增计数，时钟为SMCLK，4分频，即3MHz
+		break;
+	case 6: //la
+		TA1CTL = 6818/lvl;
+		TA1CCR1 = (unsigned int)(TA1CCR0/100.0*98.0);	//占空比98%
+		TA1CCTL1 |= OUTMOD_6;  							//Toggle/set输出模式
+		TA1CTL = TACLR + TASSEL_2 + MC_1 +ID_2;	//清TAR为0，增计数，时钟为SMCLK，4分频，即3MHz
+		break;
+	case 7: //si
+		TA1CCR0=6061/lvl;
+		TA1CCR1 = (unsigned int)(TA1CCR0/100.0*98.0);	//占空比98%
+		TA1CCTL1 |= OUTMOD_6;  							//Toggle/set输出模式
+		TA1CTL = TACLR + TASSEL_2 + MC_1 +ID_2;	//清TAR为0，增计数，时钟为SMCLK，4分频，即3MHz
+		break;
+	default:
+		break;
+	}
+}
+
+//延时函数
+void delay(unsigned char b)
+{
+	unsigned char j;
+	unsigned int i;
+	b = b*12;
+	for (j=0;j<b;j++)
+	{
+		for (i=0;i<0xffff;i++){}
+	}
 }
